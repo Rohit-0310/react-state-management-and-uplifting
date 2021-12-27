e1713ce7a895d8668b02a6a033934aedabe99def
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-27 15:39:10 +0530

    child to parents relations

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    const data = "MasaiSchool";
+    const handleData = (data) => {
+        console.log("receiver:", data);
+    }
     return (
         <div className="App">
             <h1>Child Receiverd</h1>
-            <Child_1 data={data} />
+            <Child_1 handleData={handleData}/>
         </div>
     );
 }
-function Child_1({ data }){
+function Child_1({handleData}){
+    const data = "MasaiSchool";
+    handleData(data)
     return <h3>Child Receiverd: {data}</h3>
 
 }
 
+function Child_2(){}
 
+
+
+/*
+Have a function in parents
+give it to child
+call function in child, send some data in it as arguments
+*/
 export default App;
